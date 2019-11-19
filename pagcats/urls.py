a07fbb578dafcea1892b145df79859b41439bb15
Cat <cataaza@gmail.com>
2019-11-18 22:30:17 -0300

    enlazo paginas

--- pagcats/urls.py
+++ pagcats/urls.py
@@ -1,6 +1,10 @@
+from django.conf.urls import url
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.post_list, name='post_list' ),
+    path('inicio', views.post_list, name='post_list' ),
+    path('formulario', views.post_web2, name='post_web2' ),
+    path('contacto', views.post_web3, name='post_web3' ),
 ]
--- pagcats/views.py
+++ pagcats/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+
 
 # Create your views here.
-def home(request):
+def post_list(request):
     return render(request, 'pagcats/home.html', {})
+
+def post_web2(request):
+    return render(request, 'pagcats/formulario_mascota.html', {})
+
+def post_web3(request):
+    return render(request, 'pagcats/contacto.html', {})
 
